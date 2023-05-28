a2fd00ada363d50b98bb65a9c8f600ddad6f4003
Anton Pryadkin <ap.pryadkin@dm-solutions.ru>
2023-05-28 14:17:41 +0400

    add auth, add create-user.dto, update user.service, update user.controller

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,32 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
+
+    if (user?.password !== password) {
+      const { password, ...result } = user;
+      // TODO: Generate a JWT and return it here
+      // instead of the user object
+      return result;
+    }
+
+    return null;
+  }
+
+  async register(dto: CreateUserDto) {
+    try {
+      const userData = await this.usersService.create(dto);
+
+      return userData;
+    } catch (err) {
+      console.error(err);
+      throw new ForbiddenException('Ошибка при регистрации');
+    }
+  }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,18 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({
+    default: 'pryadkin.ant@yandex.ru',
+  })
+  email: string;
+
+  @ApiProperty({
+    default: 'Anton Pryadkin',
+  })
+  fullname: string;
+
+  @ApiProperty({
+    default: 'qwerty',
+  })
+  password: string;
+}
